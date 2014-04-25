2c612e76bb0572b63f5c8d9bd69ae44eab877faf
Eduardo Soares <eduardosoares.pt@gmail.com>
2014-04-25 22:30:13 +0100

    Renamed project folders;

--- ipmanager/src/pt/it/porto/esoares/android/network/ip/IPManagerFactory.java
+++ ipmanager/src/pt/it/porto/esoares/android/network/ip/IPManagerFactory.java
@@ -4,13 +4,8 @@ package pt.it.porto.esoares.android.network.ip;
 import android.content.Context;
 import android.os.Build;
 
-public final class IPManagerFactory {
+public class IPManagerFactory {
 
-	/**
-	 * Creates a IPManager
-	 * @param context 
-	 * @return the correct IPManager for the current Android version  
-	 */
     public static IPManager getIPManager(Context context) {
         if (Build.VERSION_CODES.GINGERBREAD_MR1 >= android.os.Build.VERSION.SDK_INT) {
             return new IPManagerPreHoneycomb(context);
